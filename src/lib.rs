fd218ff67fa36ff0e71e2177664a1fec7e2127ef
vesper-arch <vesper-arch@proton.me>
2025-10-08 20:29:33 -0400

    try using the actual size of fixed elements

--- src/lib.rs
+++ src/lib.rs
@@ -456,13 +456,13 @@ pub mod clay_main {
         if current_elements[0].element.layout.sizing.width == SizingMode::Fit {
             if current_elements[0].element.layout.layout_direction == ChildLayoutDirection::LeftToRight {
                 match current_elements[1].element.layout.sizing.width {
-                    SizingMode::Fixed(size) => {current_elements[0].element.final_size_x += size as f32 },
+                    SizingMode::Fixed(_) => {current_elements[0].element.final_size_x += current_elements[1].element.final_size_x },
                     SizingMode::Fit => {},
                     SizingMode::Grow => {}
                 }
             } else {
                 match current_elements[1].element.layout.sizing.width {
-                    SizingMode::Fixed(size) => {current_elements[0].element.final_size_x = f32::max(size as f32, current_elements[0].element.final_size_x) },
+                    SizingMode::Fixed(_) => {current_elements[0].element.final_size_x = f32::max(current_elements[1].element.final_size_x, current_elements[0].element.final_size_x) },
                     SizingMode::Fit => {},
                     SizingMode::Grow => {}
                 }
@@ -471,13 +471,13 @@ pub mod clay_main {
         if current_elements[0].element.layout.sizing.height == SizingMode::Fit {
             if current_elements[0].element.layout.layout_direction == ChildLayoutDirection::LeftToRight {
                 match current_elements[1].element.layout.sizing.height {
-                    SizingMode::Fixed(size) => {current_elements[0].element.final_size_y = f32::max(size as f32, current_elements[0].element.final_size_y) },
+                    SizingMode::Fixed(_) => {current_elements[0].element.final_size_y = f32::max(current_elements[1].element.final_size_x, current_elements[0].element.final_size_y) },
                     SizingMode::Fit => {},
                     SizingMode::Grow => {}
                 }
             } else {
                 match current_elements[1].element.layout.sizing.height {
-                    SizingMode::Fixed(size) => {current_elements[0].element.final_size_y += size as f32 },
+                    SizingMode::Fixed(_) => {current_elements[0].element.final_size_y += current_elements[1].element.final_size_x },
                     SizingMode::Fit => {},
                     SizingMode::Grow => {}
                 }
