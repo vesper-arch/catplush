0360bd7932fb7f221f42be0dcf6a6435512bb38d
vesper-arch <vesper-arch@proton.me>
2025-10-23 04:47:07 +0400

    asdjaskdha

--- src/lib.rs
+++ src/lib.rs
@@ -473,10 +473,10 @@ pub mod clay_main {
         if current_elements[1].element.layout.sizing.height == SizingMode::Fit || current_elements[1].element.layout.sizing.height == SizingMode::Grow {
             match current_elements[1].element.layout.layout_direction {
                 ChildLayoutDirection::LeftToRight => {
-                    current_elements[1].element.final_size_x += (current_elements[0].element.layout.child_gap.0 * (current_elements[0].child_elements.len() as i32 - 1)) as f32
+                    current_elements[1].element.final_size_x += (current_elements[1].element.layout.child_gap.0 * (current_elements[1].child_elements.len() as i32 - 1)) as f32
                 }
                 ChildLayoutDirection::TopToBottom => {
-                    current_elements[1].element.final_size_y += (current_elements[0].element.layout.child_gap.0 * (current_elements[0].child_elements.len() as i32 - 1)) as f32
+                    current_elements[1].element.final_size_y += (current_elements[1].element.layout.child_gap.0 * (current_elements[1].child_elements.len() as i32 - 1)) as f32
                 }
             }
         }
