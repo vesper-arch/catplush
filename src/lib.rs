e6767faaae6b6657bb5f09878e62bb1c0a8238ff
vesper-arch <vesper-arch@proton.me>
2025-10-29 02:16:31 +0400

    Put all layout building functions as methods of the context object

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ pub mod clay_main {
                 open_layout_elements: vec![]
             };
 
-            open_element(&mut new_context, ClayElement::new()
+            new_context.open_element(ClayElement::new()
                 .sizing(SizingMode::Fixed(window_size.0), SizingMode::Fixed(window_size.1))
                 .layout_direction(layout_direction));
 
@@ -368,176 +368,178 @@ pub mod clay_main {
         pub id: &'static str,
     }
 
-    //////////// Finalizing Functions //////////////
+    impl ClayContext {
+        //////////// Finalizing Functions //////////////
 
-    // Solves all sizing and positioning and returns a set of render commands for passing to the
-    // renderer
-    pub fn end_layout(mut context: ClayContext) -> Vec<RenderCommand> {
-        context.open_layout_elements.clear();
+        // Solves all sizing and positioning and returns a set of render commands for passing to the
+        // renderer
+        pub fn end_layout(mut self) -> Vec<RenderCommand> {
+            self.open_layout_elements.clear();
 
-        size_layout_widths(&mut context, 0, &ChildLayoutDirection::LeftToRight);
+            self.size_layout_widths(0, &ChildLayoutDirection::LeftToRight);
 
-        position_all(&mut context, &ChildLayoutDirection::LeftToRight);
+            self.position_all(&ChildLayoutDirection::LeftToRight);
 
-        let mut render_commands: Vec<RenderCommand> = vec![];
-        for node in &context.layout_elements[1..context.layout_elements.len()] {
-            let element = &node.element;
-            let bounding_box = BoundingBox { x: element.final_pos_x, y: element.final_pos_y, width: element.final_size_x, height: element.final_size_y };
-            let render_data = match &element.object_type {
-                ElementType::Unset => RenderData::NoType,
-                ElementType::Rectangle => { RenderData::RectangleData(RectangleRenderData { color: element.color, corner_radius: element.corner_radius }) },
-                ElementType::Border(width) => { RenderData::BorderData(BorderRenderData { color: element.color, corner_radius: element.corner_radius, width: *width }) }
-                ElementType::Text(contents, font_size) => {
-                    RenderData::TextData(TextRenderData { color: element.color, font_size: *font_size, letter_spacing: 0, line_height: 0 })
-                },
-                ElementType::Image(file) => {
-                    RenderData::ImageData(ImageRenderData { tint: element.color, corner_radius: element.corner_radius })
-                }
-            };
+            let mut render_commands: Vec<RenderCommand> = vec![];
+            for node in &self.layout_elements[1..self.layout_elements.len()] {
+                let element = &node.element;
+                let bounding_box = BoundingBox { x: element.final_pos_x, y: element.final_pos_y, width: element.final_size_x, height: element.final_size_y };
+                let render_data = match &element.object_type {
+                    ElementType::Unset => RenderData::NoType,
+                    ElementType::Rectangle => { RenderData::RectangleData(RectangleRenderData { color: element.color, corner_radius: element.corner_radius }) },
+                    ElementType::Border(width) => { RenderData::BorderData(BorderRenderData { color: element.color, corner_radius: element.corner_radius, width: *width }) }
+                    ElementType::Text(contents, font_size) => {
+                        RenderData::TextData(TextRenderData { color: element.color, font_size: *font_size, letter_spacing: 0, line_height: 0 })
+                    },
+                    ElementType::Image(file) => {
+                        RenderData::ImageData(ImageRenderData { tint: element.color, corner_radius: element.corner_radius })
+                    }
+                };
 
-            let id = match element.id.clone() {
-                Some(i) => i,
-                _ => ""
-            };
+                let id = match element.id.clone() {
+                    Some(i) => i,
+                    _ => ""
+                };
+                
+                render_commands.push( RenderCommand { bounding_box, render_data, id } );
+            }
+
+            return render_commands
+        }
+
+        //////////// Layout Building Functions //////////////
+
+        pub fn open_element(&mut self, element: ClayElement) {
+            let new_element_index = self.layout_elements.len();
+            let mut parent_element: usize = 0;
             
-            render_commands.push( RenderCommand { bounding_box, render_data, id } );
-        }
-
-        return render_commands
-    }
-
-    //////////// Layout Building Functions //////////////
-
-    pub fn open_element(context: &mut ClayContext, element: ClayElement) {
-        let new_element_index = context.layout_elements.len();
-        let mut parent_element: usize = 0;
-        
-        if context.open_layout_elements.len() > 0 {
-            context.layout_elements[*context.open_layout_elements.last_mut().unwrap()].child_elements.push(new_element_index);
-            parent_element = *context.open_layout_elements.last().unwrap();
-        }
-
-        context.open_layout_elements.push(new_element_index);
-        context.layout_elements.push(Node::new(element, parent_element));
-    }
-
-    pub fn close_element(context: &mut ClayContext) {
-        // Naturally gets called in Depth First Order so we can do fixed sizing and fit sizing
-        // widths right here
-        if context.open_layout_elements.len() <= 1 {
-            return
-        }
-
-        let layout_slice = &mut context.layout_elements[..];
-        let last_opened_element = *context.open_layout_elements.last().unwrap();
-        let parent_element = layout_slice[last_opened_element].parent.unwrap();
-        // index 0: parent node | index 1: last opened node (the one being closed)
-        let current_elements = layout_slice.get_disjoint_mut([parent_element, last_opened_element]).unwrap();
-
-        // Padding
-        current_elements[1].element.final_size_x += (current_elements[1].element.layout.padding.left + current_elements[1].element.layout.padding.right) as f32;
-        current_elements[1].element.final_size_y += (current_elements[1].element.layout.padding.top + current_elements[1].element.layout.padding.bottom) as f32;
-
-        // Fixed Sizing
-        match current_elements[1].element.layout.sizing.width {
-            SizingMode::Fixed(size) => {current_elements[1].element.final_size_x = size as f32},
-            SizingMode::Fit => {},
-            SizingMode::Grow => {},
-        }
-        match current_elements[1].element.layout.sizing.height {
-            SizingMode::Fixed(size) => {current_elements[1].element.final_size_y = size as f32},
-            SizingMode::Fit => {},
-            SizingMode::Grow => {},
-        }
-
-        let child_gap = (current_elements[0].child_elements.len() as i32 - 1) * current_elements[0].element.layout.child_gap;
-
-        // Fit Sizing
-        if current_elements[0].element.layout.sizing.width == SizingMode::Fit || current_elements[0].element.layout.sizing.width == SizingMode::Grow {
-            if current_elements[0].element.layout.layout_direction == ChildLayoutDirection::LeftToRight {
-                current_elements[0].element.final_size_x += child_gap as f32;
-                current_elements[0].element.final_size_x += current_elements[1].element.final_size_x;
-            } else {
-                current_elements[0].element.final_size_x = f32::max(current_elements[1].element.final_size_x, current_elements[0].element.final_size_x)
+            if self.open_layout_elements.len() > 0 {
+                self.layout_elements[*self.open_layout_elements.last_mut().unwrap()].child_elements.push(new_element_index);
+                parent_element = *self.open_layout_elements.last().unwrap();
             }
 
+            self.open_layout_elements.push(new_element_index);
+            self.layout_elements.push(Node::new(element, parent_element));
         }
-        if current_elements[0].element.layout.sizing.height == SizingMode::Fit || current_elements[0].element.layout.sizing.height == SizingMode::Grow {
-            if current_elements[0].element.layout.layout_direction == ChildLayoutDirection::LeftToRight {
-                current_elements[0].element.final_size_y = f32::max(current_elements[1].element.final_size_y, current_elements[0].element.final_size_y);
-            } else {
-                current_elements[0].element.final_size_y += child_gap as f32;
-                current_elements[0].element.final_size_y += current_elements[1].element.final_size_y;
+
+        pub fn close_element(&mut self) {
+            // Naturally gets called in Depth First Order so we can do fixed sizing and fit sizing
+            // widths right here
+            if self.open_layout_elements.len() <= 1 {
+                return
+            }
+
+            let layout_slice = &mut self.layout_elements[..];
+            let last_opened_element = *self.open_layout_elements.last().unwrap();
+            let parent_element = layout_slice[last_opened_element].parent.unwrap();
+            // index 0: parent node | index 1: last opened node (the one being closed)
+            let current_elements = layout_slice.get_disjoint_mut([parent_element, last_opened_element]).unwrap();
+
+            // Padding
+            current_elements[1].element.final_size_x += (current_elements[1].element.layout.padding.left + current_elements[1].element.layout.padding.right) as f32;
+            current_elements[1].element.final_size_y += (current_elements[1].element.layout.padding.top + current_elements[1].element.layout.padding.bottom) as f32;
+
+            // Fixed Sizing
+            match current_elements[1].element.layout.sizing.width {
+                SizingMode::Fixed(size) => {current_elements[1].element.final_size_x = size as f32},
+                SizingMode::Fit => {},
+                SizingMode::Grow => {},
+            }
+            match current_elements[1].element.layout.sizing.height {
+                SizingMode::Fixed(size) => {current_elements[1].element.final_size_y = size as f32},
+                SizingMode::Fit => {},
+                SizingMode::Grow => {},
+            }
+
+            let child_gap = (current_elements[0].child_elements.len() as i32 - 1) * current_elements[0].element.layout.child_gap;
+
+            // Fit Sizing
+            if current_elements[0].element.layout.sizing.width == SizingMode::Fit || current_elements[0].element.layout.sizing.width == SizingMode::Grow {
+                if current_elements[0].element.layout.layout_direction == ChildLayoutDirection::LeftToRight {
+                    current_elements[0].element.final_size_x += child_gap as f32;
+                    current_elements[0].element.final_size_x += current_elements[1].element.final_size_x;
+                } else {
+                    current_elements[0].element.final_size_x = f32::max(current_elements[1].element.final_size_x, current_elements[0].element.final_size_x)
+                }
+
+            }
+            if current_elements[0].element.layout.sizing.height == SizingMode::Fit || current_elements[0].element.layout.sizing.height == SizingMode::Grow {
+                if current_elements[0].element.layout.layout_direction == ChildLayoutDirection::LeftToRight {
+                    current_elements[0].element.final_size_y = f32::max(current_elements[1].element.final_size_y, current_elements[0].element.final_size_y);
+                } else {
+                    current_elements[0].element.final_size_y += child_gap as f32;
+                    current_elements[0].element.final_size_y += current_elements[1].element.final_size_y;
+                }
+            }
+
+            self.open_layout_elements.pop();
+        }
+
+
+        pub(crate) fn size_layout_widths(&mut self, current_element: usize, layout_direction: &ChildLayoutDirection) {
+            for child in self.layout_elements[current_element].child_elements.clone() {
+                self.size_layout_widths(child, layout_direction);
             }
         }
 
-        context.open_layout_elements.pop();
-    }
-
-
-    pub(crate) fn size_layout_widths(context: &mut ClayContext, current_element: usize, layout_direction: &ChildLayoutDirection) {
-        for child in context.layout_elements[current_element].child_elements.clone() {
-            size_layout_widths(context, child, layout_direction);
-        }
-    }
-
-    pub(crate) fn position_all(context: &mut ClayContext, layout_direction: &ChildLayoutDirection) {
-        position_along_x(context, 0, layout_direction);
-        position_along_y(context, 0, layout_direction);
-    }
-
-    pub(crate) fn position_along_x(context: &mut ClayContext, current_element: usize, layout_direction: &ChildLayoutDirection) {
-        let mut child_num = 1;
-        let mut total_child_x_offset = 0.0;
-
-        for child in context.layout_elements[current_element].child_elements.clone() {
-            match context.layout_elements[current_element].element.layout.layout_direction {
-                ChildLayoutDirection::LeftToRight => {
-                    context.layout_elements[child].element.final_pos_x = total_child_x_offset
-                        + context.layout_elements[current_element].element.final_pos_x
-                        + context.layout_elements[current_element].element.layout.padding.left as f32
-                        + (context.layout_elements[current_element].element.layout.child_gap * (child_num - 1)) as f32;
-                }
-                ChildLayoutDirection::TopToBottom => {
-                    context.layout_elements[child].element.final_pos_x = context.layout_elements[current_element].element.final_pos_x
-                        + context.layout_elements[current_element].element.layout.padding.left as f32;
-                }
-            };
-
-            total_child_x_offset += context.layout_elements[child].element.final_size_x;
-            child_num += 1;
+        pub(crate) fn position_all(&mut self, layout_direction: &ChildLayoutDirection) {
+            self.position_along_x(0, layout_direction);
+            self.position_along_y(0, layout_direction);
         }
 
-        for child in context.layout_elements[current_element].child_elements.clone() {
-            position_along_x(context, child, layout_direction);
+        pub(crate) fn position_along_x(&mut self, current_element: usize, layout_direction: &ChildLayoutDirection) {
+            let mut child_num = 1;
+            let mut total_child_x_offset = 0.0;
+
+            for child in self.layout_elements[current_element].child_elements.clone() {
+                match self.layout_elements[current_element].element.layout.layout_direction {
+                    ChildLayoutDirection::LeftToRight => {
+                        self.layout_elements[child].element.final_pos_x = total_child_x_offset
+                            + self.layout_elements[current_element].element.final_pos_x
+                            + self.layout_elements[current_element].element.layout.padding.left as f32
+                            + (self.layout_elements[current_element].element.layout.child_gap * (child_num - 1)) as f32;
+                    }
+                    ChildLayoutDirection::TopToBottom => {
+                        self.layout_elements[child].element.final_pos_x = self.layout_elements[current_element].element.final_pos_x
+                            + self.layout_elements[current_element].element.layout.padding.left as f32;
+                    }
+                };
+
+                total_child_x_offset += self.layout_elements[child].element.final_size_x;
+                child_num += 1;
+            }
+
+            for child in self.layout_elements[current_element].child_elements.clone() {
+                self.position_along_x(child, layout_direction);
+            }
         }
-    }
 
 
-    pub(crate) fn position_along_y(context: &mut ClayContext, current_element: usize, layout_direction: &ChildLayoutDirection) {
-        let mut child_num = 1;
-        let mut total_child_y_offset = 0.0;
+        pub(crate) fn position_along_y(&mut self, current_element: usize, layout_direction: &ChildLayoutDirection) {
+            let mut child_num = 1;
+            let mut total_child_y_offset = 0.0;
 
-        for child in context.layout_elements[current_element].child_elements.clone() {
-            match context.layout_elements[current_element].element.layout.layout_direction {
-                ChildLayoutDirection::LeftToRight => {
-                    context.layout_elements[child].element.final_pos_y = context.layout_elements[current_element].element.final_pos_y
-                        + context.layout_elements[current_element].element.layout.padding.top as f32;
-                }
-                ChildLayoutDirection::TopToBottom => {
-                    context.layout_elements[child].element.final_pos_y = total_child_y_offset
-                        + context.layout_elements[current_element].element.final_pos_y
-                        + context.layout_elements[current_element].element.layout.padding.top as f32
-                        + (context.layout_elements[current_element].element.layout.child_gap * (child_num - 1)) as f32;
-                }
-            };
+            for child in self.layout_elements[current_element].child_elements.clone() {
+                match self.layout_elements[current_element].element.layout.layout_direction {
+                    ChildLayoutDirection::LeftToRight => {
+                        self.layout_elements[child].element.final_pos_y = self.layout_elements[current_element].element.final_pos_y
+                            + self.layout_elements[current_element].element.layout.padding.top as f32;
+                    }
+                    ChildLayoutDirection::TopToBottom => {
+                        self.layout_elements[child].element.final_pos_y = total_child_y_offset
+                            + self.layout_elements[current_element].element.final_pos_y
+                            + self.layout_elements[current_element].element.layout.padding.top as f32
+                            + (self.layout_elements[current_element].element.layout.child_gap * (child_num - 1)) as f32;
+                    }
+                };
 
-            total_child_y_offset += context.layout_elements[child].element.final_size_y;
-            child_num += 1;
-        }
+                total_child_y_offset += self.layout_elements[child].element.final_size_y;
+                child_num += 1;
+            }
 
-        for child in context.layout_elements[current_element].child_elements.clone() {
-            position_along_y(context, child, layout_direction);
+            for child in self.layout_elements[current_element].child_elements.clone() {
+                self.position_along_y(child, layout_direction);
+            }
         }
     }
 }
